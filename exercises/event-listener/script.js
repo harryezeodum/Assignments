4d542de349594eb60f4d02ca8ff04bd2ef6f7d7c
Harry Ezeodum <harryezeodum@yahoo.com>
2023-08-14 11:40:44 -0500

    Adding my Exercises/Assignments

---
+++ exercises/event-listener/script.js
@@ -0,0 +1,31 @@
+const square = document.getElementById('colorSquare');
+
+function onClicked() {
+        square.style.backgroundColor = "purple"; 
+}
+function onMouse() {
+    square.style.backgroundColor = "blue";
+}
+function onMouseDown() {
+    square.style.backgroundColor = "green";
+}
+function onMouseUp() {
+    square.style.backgroundColor = "yellow";
+}
+function doubleClicked(){
+    square.style.backgroundColor = "black";
+}
+function scrolled() {
+square.style.backgroundColor = "orange";
+}
+function keyPress() {
+    alert("Hello");
+}
+square.addEventListener('click', onClicked);
+square.addEventListener("mouseover", onMouse);
+square.addEventListener("mousedown", onMouseDown);
+square.addEventListener("mouseup", onMouseUp);
+square.addEventListener("dblclick", doubleClicked);
+square.addEventListener("scroll", scrolled);
+square.addEventListener("keypress", keyPress);
+
